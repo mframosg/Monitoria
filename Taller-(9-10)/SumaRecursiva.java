65b5f43eec4caab0caffffbe3defd2f8b0f9bb1c
Miguel Ramos <mframosg@eafit.edu.co>
2021-05-07 13:27:55 -0500

    Lambda function

--- Taller-(9-10)/SumaRecursiva.java
+++ Taller-(9-10)/SumaRecursiva.java
@@ -1,7 +1,11 @@
+import java.util.function.Function;
+
 public class SumaRecursiva {
 
     public static void main(String[] args) {
         System.out.println(sumaRecursiva(3));
+        Function<Integer, Integer> a = (num) -> {return num <= 0 ? 0 : sumaRecursiva(num - 1) + num;};
+        System.out.println(a.apply(4));
     }
 
     public static int sumaRecursiva(int num) {
